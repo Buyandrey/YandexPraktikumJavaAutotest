6b1a922678757950107e9709bbe41fe09fbdd966
Buyandrey <q4T2#*lFCGjfgsLsvDCZhy>
2021-09-30 21:45:10 +0300

    добавил класс страницы заказа

---
+++ src/main/java/ru/praktikum/yandex/OrderPage.java
@@ -0,0 +1,8 @@
+package ru.praktikum.yandex;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class OrderPage {
+}
